6c613f302f9b966399c2039add70de50e10f387a
zicoovic <abdelrhmanzakariaqa@gmail.com>
2024-02-13 14:59:15 +0300

    Solve 5 solutions

--- algorithmSource3.cpp
+++ algorithmSource3.cpp
@@ -5,6 +5,7 @@
 	#include <array>
 	#include <vector>
 	#include <cstdlib>
+	#include "algorithmLib2.h"
 	#include "MyLib.h"
 	#include "ProjectTwoLib.h"
 	#include <iomanip>
@@ -21,7 +22,7 @@
 	{
 	for (int j = 0; j < Cols; j++)
 	{
-		arr[i][j] = ProjectTwo::RandomNumber(0, 3);
+		arr[i][j] = ProjectTwo::RandomNumber(1, 100);
 	}
 	}
 	}
@@ -305,6 +306,98 @@
 		short MatrixSize = Rows * Cols;
 		return (CountNumberInMatrix( Matrix1, 0, Rows,  Cols) >= (MatrixSize / 2));
 	}
+
+	//Problem #17/03
+
+	bool CheckNumber(int Matrix1[3][3],int Number,  int Rows, int Cols) {
+			for (int i = 0; i < Rows; i++)
+			{
+				for (int j = 0; j < Cols; j++)
+				{
+					if (Number == Matrix1[i][j])
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+	}
+
+	//Problem #18/03
+
+	
+	void PrintIntersectedMatrix(int arr1[3][3], int arr2[3][3], short Rows, short Cols) {
+		int Number;
+		for (int i = 0; i < Rows; i++)
+		{
+			for (int j = 0; j < Cols; j++)
+			{
+				Number = arr1[i][j];
+
+				if (CheckNumber(arr2,Number,Rows,Cols)) {
+					cout  <<setw(3) << Number << "    ";
+            }
+			}
+		
+		}
+	}
+
+	//Problem #19/03
+
+	int PrintMinNumberInMatrix(int Matrix1[3][3],  int Rows, int Cols) {
+
+		int MinNumber  = Matrix1[0][0];
+		for (int i = 0; i < Rows; i++)
+		{
+			for (int j = 0; j < Cols; j++)
+			{
+				MinNumber  = min(MinNumber , Matrix1[i][j]);
+
+				// if (Matrix1[i][j] < MinNumber)
+				// {
+				// 	MinNumber = Matrix1[i][j];
+				// }
+			
+			}
+		
+		}
+		return MinNumber;
+	}
+
+	int PrintMaxNumberInMatrix(int Matrix1[3][3],  int Rows, int Cols) {
+
+		int MaxNumber  = Matrix1[0][0];
+		for (int i = 0; i < Rows; i++)
+		{
+			for (int j = 0; j < Cols; j++)
+			{
+
+				MaxNumber  = max(MaxNumber , Matrix1[i][j]);
+				// if (Matrix1[i][j] > MaxNumber)
+				// {
+				// 	MaxNumber = Matrix1[i][j];
+				// }
+			
+			}
+		
+		}
+		return MaxNumber;
+	}
+
+	//Problem #20/03
+
+	bool palindromeMatrix(int Matrix[3][3], int Rows, int Cols) {
+		for (int i = 0; i < Rows; i++) {
+			for (int j = 0; j < Cols / 2; j++)
+			{
+				if (Matrix[i][j] != Matrix[i][Cols - 1 - j]){
+				return false;
+			}
+			}
+		}
+
+		return true;
+	}
 	int main() {
 
 	srand((unsigned)time(NULL));
@@ -440,15 +533,60 @@
 	
 	cout << endl << "Number " << Number << " count in Matrix is " << CountNumberInMatrix(Matrix1, Number, 3, 3);*/
 	//Problem #16/03
-	cout << "\n Matrix1:\n\n";
-	FillMatrixWithRandomNumbers(Matrix1, 3, 3);
-	PrintMatrix(Matrix1, 3, 3);
-	if (IsSparseMatrix(Matrix1, 3, 3))
+	// cout << "\n Matrix1:\n\n";
+	// FillMatrixWithRandomNumbers(Matrix1, 3, 3);
+	// PrintMatrix(Matrix1, 3, 3);
+	// if (IsSparseMatrix(Matrix1, 3, 3))
+	// {
+	// 	cout << endl <<  "Yes It's Sparse";
+	// }
+	// else {
+	// 	cout << endl << "No It's Not Sparse";
+	// }
+	//Problem #17/03
+	// int Number;
+	// cout << "\n Matrix1:\n\n";
+	// FillMatrixWithRandomNumbers(Matrix1, 3, 3);
+	// PrintMatrix(Matrix1, 3, 3);
+	// cout << endl << "Enter the Number you want to check: ";
+	// cin >> Number;
+	// if (CheckNumber(Matrix1, Number, 3, 3))
+	// {
+	// 	cout << endl <<  "Yes It's there";
+	// }
+	// else {
+	// 	cout << endl << "No It's Not there";
+	// }
+
+	//Problem #18/03
+	// cout << "\n Matrix1:\n\n";
+	// FillMatrixWithRandomNumbers(Matrix1, 3, 3);
+	// PrintMatrix(Matrix1, 3, 3);
+	// cout << "\n Matrix2:\n\n";
+	// FillMatrixWithRandomNumbers(Matrix2, 3, 3);
+	// PrintMatrix(Matrix2, 3, 3);
+	// cout << "------\n";
+	// PrintIntersectedMatrix(Matrix1,Matrix2 ,3,3);
+
+	//Problem #19/03
+	// cout << "\n Matrix1:\n\n";
+	// FillMatrixWithRandomNumbers(arr, 3, 3);
+	// PrintMatrix(arr, 3, 3);
+	// cout << "This Min Number is: ";
+	// cout << PrintMinNumberInMatrix(arr,3,3);
+	// cout << "\nThis Max Number is: ";
+	// cout << PrintMaxNumberInMatrix(arr,3,3);
+	// cout << "The Palindrome Number is: ";
+	//Problem #20/03
+		cout << "\n Matrix1:\n\n";
+	FillMatrixWithRandomNumbers(arr, 3, 3);
+	PrintMatrix(arr, 3, 3);
+	if (palindromeMatrix(Matrix1,3,3))
 	{
-		cout << endl <<  "Yes It's Sparse";
+		cout << "Yes it's Palindrome" << endl;
+	}else {
+		cout << "No it's not Palindrome" << endl;
 	}
-	else {
-		cout << endl << "No It's Not Sparse";
-	}
+	
 	return 0;
 	}
